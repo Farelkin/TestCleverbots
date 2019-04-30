a6cfe388c8fbf35a58fad4b676fcdccd2b50065e
Ivan <Farelspam@yandex.ru>
2019-04-30 17:53:18 +0300

    guni update

--- gunicorn.py
+++ gunicorn.py
@@ -5,15 +5,21 @@ from os import environ
 def max_workers():
     return cpu_count()
 
-
-bind = '0.0.0.0:' + environ.get('PORT', '8000')
+user = 'root'
+group = 'www-data'
+directory = '/home/cleverbots'
 max_requests = 1000
-worker_class = 'gevent'
 workers = max_workers()
+bind = 'unix:/home/cleverbots/cleverbots.sock'
 
 env = {
-    'DJANGO_SETTINGS_MODULE': 'cleverbots.settings'
+    'DJANGO_SETTINGS_MODULE': 'cleverbots.settings',
 }
+
+# accesslog = '/home/cleverbots/logs/gunicorn-access.log'
+# errorlog = '/home/cleverbots/logs/gunicorn-error.log'
+# loglevel = 'info'
+# daemon = True
 
 reload = True
 name = 'cleverbots'
